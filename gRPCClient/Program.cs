32e7dbce9b87bfe8d22a0af379ab1232e8ad9987
sungyunahn <asy0239@gmail.com>
2024-03-17 23:04:30 +0900

    초기 커밋.

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Net.Client;
+
+namespace gRPCClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5000");
+            //var customerClient = new Customer.CustomerClient(channel);
+
+            //var clientRequested = new CustomerLookupModel { UserId = 1 };
+
+            //var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+
+
+            //Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+
+            var greeterClient = new Greeter.GreeterClient(channel);
+
+            var greeterRequested = new HelloRequest() { Name = "asyHello"};
+
+            var reply = await greeterClient.SayHelloAsync(greeterRequested);
+
+            Console.WriteLine(reply.Message);
+            Console.ReadLine();
+        }
+    }
+}
